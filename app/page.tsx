d11ffafd4043074d1aecb6a20d8a4d72c7d75dae
HasnainMughal7 <hasnainhero1@gmail.com>
2025-08-07 12:25:30 +0500

    Implemented Auth, Billing, Database Setup, Create Companion Server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,50 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
+import { recentSessions } from '@/constants'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to My SaaS App!</h1>
-      <Button>
-        Lets Get Started!
-      </Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+
+      <section className='home-section'>
+        <CompanionCard
+          id="1"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="2"
+          name="Countsy the Number Wizard"
+          topic="Derivatives & Integrals"
+          subject="Maths"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="3"
+          name="Verba the Vocabulary Builder"
+          topic="English Literature"
+          subject="Language"
+          duration={30}
+          color="#BDE7FF"
+        />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList
+          title="Recently Completed Sessions"
+          companions={recentSessions}
+          ClassNames="w-2/3 max-lg:w-full"
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,33 @@
 import Link from "next/link"
 import Navitems from "./Navitems"
+import Image from "next/image"
+import {
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs"
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <Link href="/">
                 <div className="flex items-center gap-2.5 cursor-pointer">
-                    <img src="/images/logo.svg" alt="logo" width={46} height={44} />
+                    <Image src="/images/logo.svg" alt="logo" width={46} height={44} />
                 </div>
             </Link>
             <div className="flex items-center gap-8">
                 <Navitems />
-                <p>Sign In</p>
+                <SignedOut>
+                        <SignInButton>
+                            <button className="btn-signin">
+                                Sign In
+                            </button>
+                        </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
